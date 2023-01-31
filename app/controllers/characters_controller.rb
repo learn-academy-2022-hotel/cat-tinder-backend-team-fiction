cfa41e88df77e687e7ed81781ee80a4a8c4c069a
djx-ctrl <cras.dtravis@gmail.com>
2023-01-31 12:43:04 -0800

    API Endpoints

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,22 @@
 class CharactersController < ApplicationController
+    def index
+        characters = Character.all
+        render json: characters
+    end
+  
+    def create
+        character = Character.create(character_params)
+        render json: character
+    end
+  
+    def update
+    end
+  
+    def destroy
+    end
+
+    private
+    def character_params
+        params.require(:character).permit(:name, :age, :enjoys, :image)
+    end
 end
--- spec/requests/characters_spec.rb
+++ spec/requests/characters_spec.rb
@@ -2,6 +2,39 @@ require 'rails_helper'
 
 RSpec.describe "Characters", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of characters" do
+      Character.create(
+        name: 'Johnny Bravo',
+        age: 31,
+        enjoys: 'Flexing on haters',
+        image: 'https://cdna.artstation.com/p/assets/images/images/020/372/674/large/alejandro-tio-gary-johny-bravo.jpg?1567531127'
+      )
+      get '/characters'
+
+      character = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(character.length).to eq 1
+    end
+  end
+end
+
+describe "POST /create" do
+  it "creates a character" do
+    character_params = {
+      character: {
+        name: 'Naruto',
+        age: 28,
+        enjoys: 'Ramen',
+        image: 'https://images.unsplash.com/photo-1594007759138-855170ec8dc0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmFydXRvfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+      }
+    }
+
+    post '/characters', params: character_params
+
+    expect(response).to have_http_status(200)
+
+    character = Character.first
+
+    expect(character.name).to eq 'Naruto'
   end
 end
